6087883217d111175dbedfc5f675eb3285d323fc
ADAM M SANADI <64148954+adamsanadi6@users.noreply.github.com>
2021-12-19 00:42:15 +0530

    Add files via upload

---
+++ Print_Floats_Number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class Print_Floats_Number{
+    public static void main(String args[]) throws IOException {
+      
+        //write your code here
+        Scanner sc = new Scanner(System.in);
+        float N = sc.nextFloat();
+        float M = sc.nextFloat();
+        if ((N>=1 && N<=100) && (M>=1 && M<=100))
+          System.out.printf("%.2f %.2f",N,M);
+        sc.close();
+      }
+}
